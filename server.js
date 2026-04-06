89c475b565e61671d73e8ee6f78f4f4f39397bea
Yohannes Alemu <johannesalemu01@gmail.com>
2026-04-06 09:27:25 +0300

    Add initial server setup with MongoDB connection and API routes

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const  express = require('express')
+const connectDB=require('./config/db')
+
+const userRoute=require('./routes/api/users')
+const authRoute=require('./routes/api/auth')
+const profileRoute=require('./routes/api/profile')
+const postRoute=require('./routes/api/posts')
+
 const app = express()
+
+
+//conect database
+connectDB();
+
 const PORT=process.env.PORT || 5000
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//Define Routes
+
+app.use('/api/users',userRoute)
+app.use('/api/profile',profileRoute)
+app.use('/api/posts',postRoute)
+app.use('/api/auth',authRoute)
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}!`)) 
